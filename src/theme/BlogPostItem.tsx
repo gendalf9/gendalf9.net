b5e3bf4d033280e80d7ed1ddb492fa449f59d2d4
gendalf9 <gendalf9@gmail.com>
2023-02-04 21:08:14 +0900

    * 덧글 설정 복구

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -1,12 +1,51 @@
-import React from "react";
-import BlogPostItem from "@theme-original/BlogPostItem";
-import Comment from "../components/Comments";
+import React, { useEffect, useRef } from "react";
+import OriginalBlogPostItem from "@theme-original/BlogPostItem";
+import { useColorMode } from "@docusaurus/theme-common";
 
-export default function BlogPostItemWrapper(props) {
+const utterancesSelector = "iframe.utterances-frame";
+
+function BlogPostItem(props) {
+  const { isDarkTheme } = useColorMode();
+  const utterancesTheme = isDarkTheme ? "github-dark" : "github-light";
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (!props.isBlogPostPage) return;
+
+    const utterancesEl = containerRef.current.querySelector(utterancesSelector);
+
+    const createUtterancesEl = () => {
+      const script = document.createElement("script");
+
+      script.src = "https://utteranc.es/client.js";
+      script.setAttribute("repo", "gednalf9/gendalf9.net");
+      script.setAttribute("issue-term", "pathname");
+      script.setAttribute("label", "comment");
+      script.setAttribute("theme", utterancesTheme);
+      script.crossOrigin = "anonymous";
+      script.async = true;
+
+      containerRef.current.appendChild(script);
+    };
+
+    const postThemeMessage = () => {
+      const message = {
+        type: "set-theme",
+        theme: utterancesTheme,
+      };
+
+      utterancesEl.contentWindow.postMessage(message, "https://utteranc.es");
+    };
+
+    utterancesEl ? postThemeMessage() : createUtterancesEl();
+  }, [utterancesTheme]);
+
   return (
     <>
-      <BlogPostItem {...props} />
-      <Comment />
+      <OriginalBlogPostItem {...props} />
+      {props.isBlogPostPage && <div ref={containerRef} />}
     </>
   );
 }
+
+export default BlogPostItem;
